c7f856ec9dd5f73064136289641bfda6505a6abb
Finley Williamson <finley.williamson@gmail.com>
2018-11-16 18:20:29 -0500

    first commit

---
+++ lib/routes/crmRoutes.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express'
+import { ContactController } from '../controllers/crmController'
+
+export class Routes {
+  
+  public contactController: ContactController = new ContactController();
+
+  public routes(app): void {
+
+    app.route('/')
+    .get((req: Request, res: Response) => {
+      res.status(200).send({
+        message: 'GET Request successful!'
+      })
+    })
+
+    // Contact
+    app.route('/contact')
+      // GET endpoint
+      .get(this.contactController.getContacts)
+      // POST endpoint
+      .post(this.contactController.addNewContact)
+
+    // Contact detail
+    app.route('/contact/:contactId')
+      // Get specific contact
+      .get(this.contactController.getContactWithID)
+      // Update a specific contact
+      .put(this.contactController.updateContact)
+      // Delete a specific contact
+      .delete(this.contactController.deleteContact)
+  }
+}
